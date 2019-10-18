57f6638a66325913d5151384ea3283b838f357d0
Benjamin25116 <benjamin25116@gmail.com>
2019-10-18 17:04:26 +0800

    80% done. Added videos and hero.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,82 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import Subscribe from "../components/subscribe"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import LinkedBanner from "../components/linkedBanner"
+import styled from "styled-components"
+
+const TextContainer = styled.article`
+  padding: 40px;
+`
+const TextBody = styled.p`
+  text-align: center;
+  font-weight: bold;
+  color: var(--bright);
+`
+const SectionTitle = styled.h2`
+  color: var(--black);
+  font-size: 1.8rem;
+  padding: 40px;
+  text-align: center;
+  margin: 0;
+`
+const VideoTitle = styled.h3`
+  color: var(--bright);
+  font-size: 1.4rem;
+  text-align: center;
+  margin: 0;
+`
+const Video = styled.iframe`
+  width: 100vw;
+  height: calc(100vw / 16 * 9);
+  border: none;
+  box-shadow: 0 1px 4px var(--black);
+  margin-bottom: 20px;
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Time to rebuild this site</h1>
-    <p>It's going to be a great one!</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <LinkedBanner
+      imageName="holy-family-1.png"
+      title="A Montessori Approach to Spiritual Education"
+      body="Godly Play encourages children to listen for God and be authentic as well as creative in their response to God's call on their lives."
+      buttonName="Learn More"
+      linkTo="/what-is-godly-play"
+    />
+    <TextContainer>
+      <TextBody>
+        Godly Play is a creative and imaginative approach to Christian education
+        and spiritual guidance.
+      </TextBody>
+    </TextContainer>
+    <LinkedBanner
+      imageName="holy-family-2.png"
+      title="Godly Play Models Adult Worship Life"
+      body="Children experience gathering together, praying, listening to stories and understanding symbols as well as participating in traditions of Christian congregations."
+      buttonName="Read More"
+      linkTo="/what-is-godly-play"
+    />
+    <SectionTitle>Watch a story being told</SectionTitle>
+    <VideoTitle>The Advent</VideoTitle>
+    <Video
+      src="https://www.youtube.com/embed/zgbJIEkZYvQ"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></Video>
+    <VideoTitle>The Great Pearl</VideoTitle>
+    <Video
+      src="https://www.youtube.com/embed/h-b3T7HfXYY"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></Video>
+    <VideoTitle>The Ten Best Ways</VideoTitle>
+    <Video
+      src="https://www.youtube.com/embed/hkjXX2xjTkg"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></Video>
+    <Subscribe />
   </Layout>
 )
 
