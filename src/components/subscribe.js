2c1e1b860b72bbad9584ceeb931046d93ef68077
Benjamin25116 <benjamin25116@gmail.com>
2019-10-18 17:04:33 +0800

    Initial commit

---
+++ src/components/subscribe.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Button from "./button"
+
+const Subscribe = () => (
+  <div>
+    <form action="">
+      <label htmlFor=""></label>
+      <input type="email" />
+      <Button buttonName="Subscribe" />
+    </form>
+  </div>
+)
+
+export default Subscribe
