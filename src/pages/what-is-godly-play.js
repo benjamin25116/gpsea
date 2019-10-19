a2c2476a3ef6385136087a277311f0506074a1e1
Benjamin25116 <benjamin25116@gmail.com>
2019-10-19 21:35:40 +0800

    Initial CSS

--- src/pages/what-is-godly-play.js
+++ src/pages/what-is-godly-play.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import Subscribe from "../components/subscribe"
 import styled from "styled-components"
+import css from "./what.module.css"
 
 const Title = styled.h1`
   color: var(--black);
@@ -11,10 +12,14 @@ const Title = styled.h1`
 `
 const Paragraph = styled.p`
   color: var(--black);
-  margin: 20px 15px;
+  margin: 0 15px 20px 15px;
   font-size: 1.15rem;
   line-height: 1.7rem;
 `
+const imageStyle = {
+  width: "100%",
+  marginBottom: "20px",
+}
 
 const WhatIsGodlyPlay = () => (
   <Layout>
@@ -27,15 +32,21 @@ const WhatIsGodlyPlay = () => (
       learn and discover truths about his environment through experience. And
       all this happens at his own pace; we cannot force a child to learn.
     </Paragraph>
-    <Image originalName="david.png" />
-    <Paragraph>
-      In Godly Play, the same principle applies. Godly Play believes that every
-      child has thoughts and questions on things spiritual. A child desires to
-      know and understand spirituality as a fact of the universe. Godly Play
-      facilitates this by setting up an environment and process whereby children
-      can play with stories in a meaningful way that leads to insight, thinking
-      about the story and engaging with Scripture.
-    </Paragraph>
+    <div className={css.wrapper}>
+      <Image
+        originalName="david.png"
+        style={imageStyle}
+        className={css.image1}
+      />
+      <Paragraph className={css.para2}>
+        In Godly Play, the same principle applies. Godly Play believes that
+        every child has thoughts and questions on things spiritual. A child
+        desires to know and understand spirituality as a fact of the universe.
+        Godly Play facilitates this by setting up an environment and process
+        whereby children can play with stories in a meaningful way that leads to
+        insight, thinking about the story and engaging with Scripture.
+      </Paragraph>
+    </div>
     <Paragraph>
       With a foundation in Montessori and a history of over 40 years of research
       and practice, Godly Play allows children to make relevant and personal
@@ -43,7 +54,11 @@ const WhatIsGodlyPlay = () => (
       relationships, and nurtures participants into a deeper dimension of belief
       and faith through wondering and play.
     </Paragraph>
-    <Image originalName="judyth.png" />
+    <Image
+      originalName="judyth.png"
+      className={css.image2}
+      style={imageStyle}
+    />
     <Paragraph>
       Employing the Godly Play method will change the way you and your
       congregation interact with children. It is an amazing spiritual journey
