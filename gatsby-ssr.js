2449d847291f100f61249c2aa6829dae27dcc474
Benjamin25116 <benjamin25116@gmail.com>
2019-10-24 18:33:50 +0800

    Added Netlify Identity and Google-tag.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from "react"
+export function onRenderBody({ setHeadComponents }) {
+  setHeadComponents([
+    <script
+      type="text/javascript"
+      src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+    />,
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-150488677-1"
+    />,
+  ])
+}
