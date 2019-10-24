6184eee551d189424ba426d04d369c522d475463
Benjamin25116 <benjamin25116@gmail.com>
2019-10-25 00:34:47 +0800

    CreatePages; buggy

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,49 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(
+    `./src/pages/templates/blogpost-template.js`
+  )
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            id
+          }
+        }
+      }
+    }
+  `)
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: `/blog${node.fields.slug}`,
+      component: blogPostTemplate,
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        id: node.id,
+      },
+    })
+  })
+}
---
+++ src/pages/templates/gallery-template.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const GalleryTemplate = () => {
+  return <div>This is the gallery</div>
+}
+
+export default GalleryTemplate
