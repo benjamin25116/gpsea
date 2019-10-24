dd96062f324f88ac5e32d5ea4d9eff94b0de14b8
Benjamin25116 <benjamin25116@gmail.com>
2019-10-24 19:15:13 +0800

    Using Netlify Script Injection

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,41 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-import React from "react"
-export function onRenderBody({ setHeadComponents, setPostBodyComponents }) {
-  setHeadComponents([
-    <script
-      type="text/javascript"
-      src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-    />,
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=UA-150488677-1"
-    />,
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-150488677-1');
-   	 `,
-      }}
-    />,
-  ])
-  setPostBodyComponents([
-    <script
-      dangerouslySetInnerHTML={{
-        __html: ` if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }`,
-      }}
-    />,
-  ])
-}
+// import React from "react"
+// export function onRenderBody({ setHeadComponents, setPostBodyComponents }) {
+//   setHeadComponents([
+//     <script
+//       type="text/javascript"
+//       src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+//     />
+//   ])
+// }
